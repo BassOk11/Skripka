9a18c37109d2faaa658514528c6332481156be88
Marsel <marsel,gafiev@gmail.com>
2024-07-18 17:50:21 +0300

    sekond commit4

---
+++ module_2_3.py
@@ -0,0 +1,23 @@
+mi_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+ind = 0
+while ind < len(mi_list):
+    element = mi_list[ind]
+    if element < 0:
+        break
+    if element > 0:
+        print(element)
+    ind += 1
+
+
+
+
+
+
+
+
+
+
+
+
+
+
