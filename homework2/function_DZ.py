7c9564c15b17602e70b4ddfa7325515993b1ca4d
Marsel <marsel,gafiev@gmail.com>
2024-08-01 14:05:30 +0300

    sekond commit4

---
+++ homework2/function_DZ.py
@@ -0,0 +1,12 @@
+def get_matrix(n=input(), m=input(), value=input()):
+    matrix = []
+    for i in (0, n):
+        matrix1 = []
+        matrix.append(matrix1)
+        for j in (0, m):
+            matrix1.append(value)
+    return matrix
+
+
+resalt = get_matrix()
+print(resalt)
