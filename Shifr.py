5ad5f2eddc5a864c77d9666d8867e69e5da50b3a
Marsel <marsel,gafiev@gmail.com>
2024-08-07 11:08:32 +0300

    sekond commit4

--- Shifr.py
+++ Shifr.py
@@ -1,11 +1,14 @@
-#n = int(input('Введите любое число от 3 до 20: '))
-a = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19]
+import random
+number = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+c = random.choice(number)
+print(c)
 b = 0
-cic = []
-for i in range(1, 20):
-    para = str(i) + str(a[b])
-    print(para)
-
+passw = ""
+for i in range(1, c):
+    for j in range(i + 1, c):
+        if c % (i + j) == 0:
+            passw += str(i) + str(j)
+print(passw)
 
 
 
