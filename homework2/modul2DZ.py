3499a4ce87c28d956227833da484d306d8e8d2d2
Marsel <marsel,gafiev@gmail.com>
2024-08-13 12:10:24 +0300

    sekond commit4

--- homework2/modul2DZ.py
+++ homework2/modul2DZ.py
@@ -1,15 +1,18 @@
 calls = 0
+
+
 def count_calls():
     global calls
     calls += 1
+
     def string_info(string):
         global calls
         calls += 1
         print((len(string), string.upper(), string.lower()))
+
     string = input('Строчка с вас: ')
 
     string_info(string)
-
 
     def is_contains():
         global calls
@@ -17,13 +20,11 @@ def count_calls():
         list_ = []
         string = input("Is contains: ")
         list_.append(input("Is_contains: "))
-        for i in range(len(list_)):
-            for j in range(len(string)):
-                print(i == j)
+        print(string in list_)
 
         print(calls)
 
-
     is_contains()
+
 
 count_calls()
